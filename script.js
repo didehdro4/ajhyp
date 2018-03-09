8a092e6cc6dfe34c9af13c0dff28d9f69e8875a6
Andy Yang <yangj079@newschool.edu>
2018-03-09 12:38:52 -0500

    b
    b

--- script.js
+++ script.js
@@ -10,9 +10,27 @@ $('.lazy').slick({
 				
 $('.preview').slick({
   infinite: true,
-  slidesToShow: 5,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  appendArrows: false
+});
+
+$('#categories').slick({
+  infinite: true,
+  slidesToShow: 6,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 1000,
+  arrows: true,
+  appendArrows: false
 });
-				
+
+
+(function($){
+        $(window).on("load",function(){
+            $(".content").mCustomScrollbar();
+        });
+    })(jQuery);
